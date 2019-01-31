36cb616fb7a9e8b08f366c3ab8e80d13307b5f76
zhengjinhuang <zhenjinhuang@epweike.com>
2019-01-31 10:44:17 +0800

    init library

---
+++ Example/Pods/Target Support Files/Pods-zpodpod_Tests/Pods-zpodpod_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_zpodpod_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_zpodpod_Tests
+@end
---
+++ Example/Pods/Target Support Files/zpodpod/zpodpod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_zpodpod : NSObject
+@end
+@implementation PodsDummy_zpodpod
+@end
